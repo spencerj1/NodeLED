fdd555686702062ba1a135ece9a975b2feb4e9d4
James Spencer <jamesspencer@Jamess-MacBook-Pro-4.local>
2019-05-09 21:08:17 -0400

    simplifying the project

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var bodyParser = require('body-parser');
 var app = express();
+var port = process.env.PORT || 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function (req, res) { 
+res.status(200).sendFile('./index.html', { root: __dirname })
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/css/business-casual.css', function (req, res) { 
+res.status(200).sendFile('./css/business-casual.css', { root: __dirname })
 });
 
-module.exports = app;
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('App listening ' + port);
+});
